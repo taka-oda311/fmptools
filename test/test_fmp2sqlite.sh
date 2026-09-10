367582526e9b0036cffd910122ddfbdcbf51cb1a
taka-oda311 <oda.work@gmail.com>
2026-09-10 10:58:09 +0900

    Add fmp2sqlite regression test

---
+++ test/test_fmp2sqlite.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+set -eu
+
+TMP_DB="${TMPDIR:-/tmp}/fmptools-portablecode39.sqlite"
+rm -f "$TMP_DB"
+
+./fmp2sqlite test/data/fmp12/PortableCode39-2.fmp12 "$TMP_DB"
+
+if [ ! -f "$TMP_DB" ]; then
+    echo "SQLite output was not created"
+    exit 1
+fi
+
+TABLES=$(sqlite3 "$TMP_DB" ".tables")
+
+echo "$TABLES" | grep -q "Data"
+
+rm -f "$TMP_DB"
+
+echo "fmp2sqlite regression test passed"
